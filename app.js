e003f22c3dc5f7c26f31f78b16feb91b7adb6f4b
liuyanzhen <liuyanzhen@baidu.com>
2021-10-26 19:25:40 +0800

    在dev里修改

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get('/list', (req, res) => {
     ];
     res.header("Access-Control-Allow-Origin", "*");
     res.json(list);
+    console.log('在main里修改，这里是dev')
 })
 
 app.listen('2021', () => {
